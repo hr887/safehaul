b758df8d644c84a90790bd1b62c9d2fe4acb6326
hr887 <hr@raystarllc.com>
2025-12-01 19:26:44 +0500

    nnnn

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['55d132c2-84b6-477f-ae6c-24518b00964f-00-2ommgstultd8a.pike.replit.dev'],
+    allowedHosts: true,
   },
 })
